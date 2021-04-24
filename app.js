50fc715ebdee3b86240017528328fa42b99fc337
sagorahmeed <sagorahmeed6060@gmail.com>
2021-04-24 19:11:11 +0600

    update navbar esign

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function(){
     var currentScrollpos = window.pageYOffset ;
     if(prevScrollpos > currentScrollpos){
-        document.getElementById('navbar').style.top = '0';
+        document.getElementById('slideNav').style.top = '0';
     }
     else{
-        document.getElementById('navbar').style.top = '-100px';
+        document.getElementById('slideNav').style.top = '-100px';
     }
     prevScrollpos = currentScrollpos;
 }
 
-document.getElementById
+// document.getElementById
 
+
+
+// var menuIcon = document.querySelector('navbarIcon');
+
+// menuIcon.addEventListener('click', function(){
+//     var menu = document.querySelector('navbar');
+//     if(menu.style.display === 'none'){
+//         menu.style.display = 'block'
+//     }else{
+//         menu.style.display = 'none'
+//     }
+// })
 
